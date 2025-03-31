a83348184b431f664112724e9662fc346e6b156b
jannathshaik123 <jannathshaik4@gmail.com>
2025-03-31 23:23:42 +0400

    updated data path

--- code/preprocessing/preprocess2.py
+++ code/preprocessing/preprocess2.py
@@ -403,7 +403,7 @@ if __name__ == "__main__":
         
         
     # Define file paths
-    base_path = os.path.join(os.path.dirname(os.getcwd()), "data")
+    base_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "data")
     reviews_path = os.path.join(base_path, "yelp_training_set/yelp_training_set_review.json")
     business_path = os.path.join(base_path, "yelp_training_set/yelp_training_set_business.json")
     user_path = os.path.join(base_path, "yelp_training_set/yelp_training_set_user.json")
