46dabe11f93bdd9485a94bed238b06e3bb4d5988
jannathshaik123 <jannathshaik4@gmail.com>
2025-04-03 12:53:16 +0400

    recommiting the new code

--- code/collaborative_filtering.py
+++ code/collaborative_filtering.py
@@ -7,10 +7,8 @@ from sklearn.model_selection import train_test_split
 
 # Paths to your data and models
 base_path = os.path.join(os.path.dirname(os.getcwd()), "data/processed")
-
-models_path = os.path.join(os.path.dirname(os.getcwd()), 'models')
-item_based_model_path = os.path.join(models_path,'item_based_cf_model.pkl')
-svd_model_path = os.path.join(models_path,'svd_model.pkl')
+item_based_model_path = 'item_based_cf_model.pkl'
+svd_model_path = 'svd_model.pkl'
 
 print("Loading models and data...")
 
